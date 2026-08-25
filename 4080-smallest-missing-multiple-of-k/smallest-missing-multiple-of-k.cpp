b2cedcaac1091f4338c24b86362a7068fc2869b9
shubhi629 <shubhisingh1906@gmail.com>
2026-08-25 09:53:22 +0530

    Time: 5 ms (15.61%) | Memory: 24.1 MB (67.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n= nums.size();
-        unordered_set<int>st;
-        for(int i=0;i<n;i++){
-            if(nums[i]%k==0){
-               st.insert(nums[i]); 
+        int n = nums.size();
+        unordered_set<int> st;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] % k == 0) {
+                st.insert(nums[i]);
             }
         }
-        
-            for(int i=k;i<=k*1000;i+=k){
-                if(st.find(i)==st.end()){
-                    return i;
-                }
-            }
-        return -1;
-        
+
+        int i = k;
+        while(st.find(i) != st.end()) {
+            i += k;
+        }
+        return i;
     }
 };
